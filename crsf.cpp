dd6441ea2f79ff10ece672478e0f60f6a5970714
Cory Douthat <soldierforchrist2@gmail.com>
2023-03-27 01:03:56 -0400

    Fix RC channel unpacking function and also include Connor's version.

--- crsf.cpp
+++ crsf.cpp
@@ -12,13 +12,15 @@ uint8_t crsf_crc(const uint8_t* buf, uint8_t len) {
   return crc;
 }
 
-// Returns frame type
-CRSFFrameType CRSFInterface::decodeFrame(uint8_t* buf, unsigned int len) {
+CRSFFrameStatus CRSFInterface::decodeFrame(uint8_t* buf, unsigned int len, CRSFFrameType* type_rtn = nullptr) {
   uint8_t addr = buf[0];
   uint8_t frame_length = buf[1];  // Includes type, but not addr/crc
   uint8_t payload_size = frame_length - 1;
   uint8_t type = buf[2];
   uint8_t crc = buf[len - 1];
+
+  if (type_rtn)
+    *type_rtn = CRSF_FRAMETYPE_ERROR;
 
   if (addr != 0xC8)
     return CRSF_FRAME_ERROR_ADDRESS;
@@ -35,7 +37,13 @@ CRSFFrameType CRSFInterface::decodeFrame(uint8_t* buf, unsigned int len) {
       if (payload_size != CRSF_FRAME_RC_CHANNELS_PAYLOAD_SIZE)
         return CRSF_FRAME_ERROR_PAYLOAD;
 
-      return decodeRCChannels(&buf[3], 16, 11);
+      if (decodeRCChannels(&buf[3], 16, 11))
+      {
+        *type_rtn = CRSF_FRAMETYPE_RC_CHANNELS_PACKED;
+        return CRSF_FRAME_VALID;
+      }
+      else
+        return CRSF_FRAME_ERROR_PAYLOAD;
 
       break;
 
@@ -45,19 +53,57 @@ CRSFFrameType CRSFInterface::decodeFrame(uint8_t* buf, unsigned int len) {
   }
 }
 
-bool CRSFInterface::decodeRCChannels(uint8_t* payload, unsigned int channel_count, unsigned int bits) {
+bool CRSFInterface::unpackRCChannels(uint8_t* data, unsigned int channel_count, unsigned int bits) {
   // TODO: check for valid payload size and number of bits
+  
+  if (!data)
+    return false;
+
+  if (bits > 16)
+    return false;
 
   unsigned int byte_start = 0;
   unsigned int bit_start = 0;
 
-  for (unsigned int ch = 0; ch < channel_count; ch++) {
-    rc_channels[ch] = (payload[byte_start]|payload[byte_start+1]<<bit_start) & uint8_t(bits * 8 - 1);
+  uint16_t temp;
 
+  // Extract channels
+  for (unsigned int ch = 0; ch < channel_count; ch++) {
+    // Concatenate bytes
+    if (byte_start < (channel_count * bits / 8 - 1))
+      temp = (data[byte_start] << 8) | data[byte_start+1];
+    else 
+      temp = (data[byte_start] << 8) | 0x00;
+    
+    // Bit shift to get relevant data in least significant bits
+    temp = temp << bit_start;
+    temp = temp >> (16 - bits);
+    rc_channels[ch] = temp;
+
+    // Track where we're at
     bit_start += bits;
-    byte_start += bit_start / bits;
-    bit_start = bit_start % bits;
+    byte_start += bit_start / 8;
+    bit_start = bit_start % 8;
   }
 
   return true;
 }
+
+// Connor's version
+// void unpack_channels(uint8_t* data, size_t bits_per_channel, uint16_t* channels, size_t num_channels) {
+//     for (size_t i_ch = 0; i_ch < num_channels; i_ch++) {
+//         channels[i_ch] = 0;
+//         size_t i_bit = i_ch * bits_per_channel;
+//         for (size_t channel_bit = 0; channel_bit < bits_per_channel;) {
+//             size_t read_byte = (i_bit + channel_bit) / 8;
+//             size_t read_byte_bit = (i_bit + channel_bit) % 8;
+//             size_t read_byte_bits = 8 - read_byte_bit;
+//             if (channel_bit + read_byte_bits > bits_per_channel) {
+//                 read_byte_bits = bits_per_channel - channel_bit;
+//             }
+//             uint8_t masked_byte = (data[read_byte] >> (8 - read_byte_bits - read_byte_bit)) & ((1 << read_byte_bits) - 1);
+//             channels[i_ch] |= masked_byte << (bits_per_channel - read_byte_bits - channel_bit);
+//             channel_bit += read_byte_bits;
+//         }
+//     }
+// }
